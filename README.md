d6f66124c5acac09ee559e9ce0af2411faaea9ea
polykernel <81340136+polykernel@users.noreply.github.com>
2024-10-18 23:26:08 -0400

    feat: add github action to build packages
    - Add GitHub Action workflow to build cachable
    packages in the package set
    - Update README.md with badges

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Polypkgs
 
-An auxiliary Nix package set.
+![Build Status](https://github.com/polykernel/polypkgs/workflows/Build%20packages/badge.svg)
+[![Cachix Cache](https://img.shields.io/badge/cachix-polypkgs-blue.svg)](https://polypkgs.cachix.org)
 
+An auxiliary package set.
--- flake.nix
+++ flake.nix
@@ -6,21 +6,29 @@
     nixpkgs.follows = "dream2nix/nixpkgs";
   };
 
-  outputs = {
-    self,
-    dream2nix,
-    nixpkgs,
-  }: let
-    supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
+  outputs =
+    {
+      self,
+      dream2nix,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-    eachSystem = nixpkgs.lib.genAttrs supportedSystems;
-  in {
-    packages = eachSystem (system:
-      dream2nix.lib.importPackages {
-        projectRoot = ./.;
-        projectRootFile = "flake.nix";
-        packagesDir = ./packages;
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-      });
-  };
+      eachSystem = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = eachSystem (
+        system:
+        dream2nix.lib.importPackages {
+          projectRoot = ./.;
+          projectRootFile = "flake.nix";
+          packagesDir = ./packages;
+          packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+        }
+      );
+    };
 }
