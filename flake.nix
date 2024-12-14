dae2798dbe5e32513bab63e0db14038d642ca88d
polykernel <81340136+polykernel@users.noreply.github.com>
2024-12-14 13:14:58 -0500

    feat: set up git-hooks and treefmt
    - Use cachix/git-hooks.nix to setup pre-commit formatting hooks
    - Add treefmt.toml
    - Format existing source files

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     dream2nix.url = "github:nix-community/dream2nix";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.follows = "dream2nix/nixpkgs";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
@@ -12,6 +13,7 @@
       self,
       dream2nix,
       flake-utils,
+      pre-commit-hooks,
       nixpkgs,
       ...
     }@inputs:
@@ -23,14 +25,49 @@
 
       lib = nixpkgs.lib // builtins;
 
-      perSystem = system: {
-        packages = flake-utils.lib.flattenTree (
-          import ./packages.nix {
-            inherit lib dream2nix;
-            pkgs = nixpkgs.legacyPackages.${system};
-          }
-        );
-      };
+      perSystem =
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { };
+            overlays = [ ];
+          };
+        in
+        lib.fix (_self: {
+          apps = lib.mapAttrs (_: drv: flake-utils.lib.mkApp { inherit drv; }) _self.packages;
+
+          packages = flake-utils.lib.flattenTree (
+            import ./packages.nix {
+              inherit lib dream2nix pkgs;
+            }
+          );
+
+          checks = {
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                treefmt.enable = true;
+                treefmt.settings = {
+                  formatters = [
+                    pkgs.nixfmt-rfc-style
+                    pkgs.typos
+                    pkgs.toml-sort
+                  ];
+                };
+              };
+            };
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              inherit (_self.checks.pre-commit-check) shellHook;
+
+              nativeBuildInputs = _self.checks.pre-commit-check.enabledPackages;
+
+            };
+          };
+        });
 
       modulesDir = ./modules;
 
